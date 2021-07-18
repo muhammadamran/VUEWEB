f22a28118dcefe3d66f8f5d7573fe60a2980a2cf
amranhakim10 <amranhakim9@gmail.com>
2021-07-19 00:15:49 +0700

    Initial commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contact">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Contact"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Contact',
+  components: {
+    HelloWorld
+  }
+}
+</script>
